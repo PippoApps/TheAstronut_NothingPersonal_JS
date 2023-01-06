181c154382e53a915b6d05385173287fb1b28cce
PippoApps <pippo@pippoapps.com>
2023-01-06 12:22:22 +0100

    Tolti file ionutili

--- js/test5.js
+++
@@ -1,9 +0,0 @@
-function loadImage() {
-  var preload = new createjs.LoadQueue();
-  preload.addEventListener("fileload", handleFileComplete);
-  preload.loadFile("../../apo/img/frame001-low.jpg");
-}
-
-function handleFileComplete(event) {
-  document.body.appendChild(event.result);
-}
